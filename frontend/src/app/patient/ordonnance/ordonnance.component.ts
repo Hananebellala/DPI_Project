6b6a981f2f58b930c352f5ece45eff61648c0e96
Hananebellala <hananebellala17@gmail.com>
2024-12-31 02:56:55 +0100

    Here will be frontend merged

--- frontend/src/app/patient/ordonnance/ordonnance.component.ts
+++ frontend/src/app/patient/ordonnance/ordonnance.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-ordonnance',
-  imports: [],
+  selector: 'ordonnance-page',
   templateUrl: './ordonnance.component.html',
-  styleUrl: './ordonnance.component.css'
+  styleUrls: ['./ordonnance.component.css'],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatIconModule
+  ]
 })
-export class OrdonnanceComponent {
+export class OrdonnanceComponent{
+  // Array to store added medications
+  medications: { name: string; dose: number; frequency: number; duration: number }[] = [];
 
+  constructor(private dialog: MatDialog) {}
+
+  
+
+  // Helper function to validate medication data
+  isValidMedication(medication: { name: string; dose: number; frequency: number; duration: number }): boolean {
+    // Ensure all fields are filled and dose, frequency, and duration are numbers
+    return (
+      medication.name.trim() !== '' &&
+      !isNaN(medication.dose) &&
+      !isNaN(medication.frequency) &&
+      !isNaN(medication.duration)
+    );
+  }
+
+  
 }
--- frontend/src/app/patient/patient-layout/patient-layout.component.ts
+++ frontend/src/app/patient/patient-layout/patient-layout.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-patient-layout',
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './patient-layout.component.html',
   styleUrl: './patient-layout.component.css'
 })
-export class PatientLayoutComponent {
+export class DoctorLayoutComponent {
 
 }
