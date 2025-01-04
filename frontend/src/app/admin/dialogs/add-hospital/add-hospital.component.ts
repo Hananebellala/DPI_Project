777814b28fa2fa6bbdd7d7ab6fea48397b7fd643
Hananebellala <hananebellala17@gmail.com>
2025-01-04 16:15:34 +0100

    half integration

--- frontend/src/app/admin/dialogs/add-hospital/add-hospital.component.ts
+++ frontend/src/app/admin/dialogs/add-hospital/add-hospital.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
@@ -7,7 +7,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
@@ -16,26 +17,62 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   styleUrls: ['./add-hospital.component.css'],
   standalone:true,
   imports:[FormsModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatDialogModule,
-    MatButtonModule,
-  MatSelectModule,
-MatIconModule]
+          MatInputModule,
+          MatFormFieldModule,
+          MatDialogModule,
+          MatButtonModule,
+          MatSelectModule,
+          MatIconModule]
 })
 export class AddHospitalComponent {
   hospitalData = {
     hospital:''
   };
 
-  
-  constructor(private dialogRef: MatDialogRef<AddHospitalComponent>) {}
+  ngOnInit() {
+    const dialogContainer = document.querySelector('.mat-dialog-container');
+    if (dialogContainer) {
+      dialogContainer.classList.add('custom-dialog');
+    }
+  }
 
+  // Get CSRF Token from cookies
+  private getCSRFToken(): string {
+    const name = 'csrftoken=';
+    const value = document.cookie.split(';').find(cookie => cookie.trim().startsWith(name))?.split('=')[1];
+    return value || '';
+  }
 
-  
-  addMember() {
-    // Logic to add the member
-    console.log('Member added:', this.hospitalData);
+  @ViewChild('hopital') hopital!: ElementRef;
+  constructor(private http: HttpClient) {}
+
+  private dialogRef!: MatDialogRef<AddHospitalComponent>;
+
+  addHopital(event: Event): void  {
+    event.preventDefault();
+
+    var hopital = this.hopital.nativeElement.value.trim();
+    var formData = new FormData();
+    formData.append('nom', hopital); 
+
+    // Extract CSRF token and include it in the headers
+        const csrfToken = this.getCSRFToken();
+        const headers = new HttpHeaders({
+          'X-CSRFToken': csrfToken, // Add CSRF token to headers
+        });
+        this.http.post('http://127.0.0.1:8000/hopital/', formData, {headers,
+          withCredentials: false}).subscribe(
+          response => {
+            console.log('Hôpital ajouté!', response);
+            alert('Hôpital ajouté avec succès.');
+          },
+          error => {
+            console.error('Erreur dans l\'ajout de l\'Hôpital', error);
+            alert('Erreur dans l\'ajout de l\'Hôpital.');
+          }
+        );
+    
+    console.log('Hopital added:', this.hospitalData);
     // Reset the form after submission
     this.hospitalData = { hospital:'' };
   }
